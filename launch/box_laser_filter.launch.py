25e5fa46c5edc71490d3b8d31af0bbf2d978a687
Gio <alfonsusgiovanni.chrisanto@wingscorp.com>
2023-03-21 16:05:51 +0700

    Change Laser Filter Launch File

--- launch/box_laser_filter.launch.py
+++ launch/box_laser_filter.launch.py
@@ -1,18 +1,20 @@
-from launch import LaunchDescription
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.actions import Node
+import os
+
 from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package="laser_filters",
-            executable="scan_to_scan_filter_chain",
-            parameters=[
-                PathJoinSubstitution([
-                    get_package_share_directory("agv_ros"),
-                    "config", "box_laser_filter.yaml",
-                ])],
+
+    box_filter_params = os.path.join(get_package_share_directory('agv_ros'),'config','box_laser_filter.yaml')
+    box_filter = Node(package="laser_filters", executable="scan_to_scan_filter_chain",
+                      parameters=[box_filter_params],
         )
+
+    return LaunchDescription([
+        box_filter
     ])
