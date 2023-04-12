dcb1586be626a1304cb32fe5fdbc33c6e57e0e84
Alfonsus Giovanni Chrisanto <alfonsusgiovanni.chrisanto@wingscorp.com>
2023-04-12 11:16:09 +0700

    Update to Humble

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -15,40 +15,44 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
 
-    package_name='agv_ros'
+    package_name = 'agv_ros'
 
     rsp = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(
+                package_name), 'launch', 'rsp.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
     )
 
     joystick = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory(package_name),'launch','joystick.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true'}.items()
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory(
+                package_name), 'launch', 'joystick.launch.py'
+        )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    twist_mux_params = os.path.join(get_package_share_directory(package_name),'config','twist_mux.yaml')
+    twist_mux_params = os.path.join(get_package_share_directory(
+        package_name), 'config', 'twist_mux.yaml')
     twist_mux = Node(
-            package="twist_mux",
-            executable="twist_mux",
-            parameters=[twist_mux_params, {'use_sim_time': True}],
-            remappings=[('/cmd_vel_out','/diff_cont/cmd_vel_unstamped')]
-        )
+        package="twist_mux",
+        executable="twist_mux",
+        parameters=[twist_mux_params, {'use_sim_time': True}],
+        remappings=[('/cmd_vel_out', '/diff_cont/cmd_vel_unstamped')]
+    )
 
-    gazebo_params_file = os.path.join(get_package_share_directory(package_name),'config','gazebo_params.yaml')
+    gazebo_params_file = os.path.join(get_package_share_directory(
+        package_name), 'config', 'gazebo_params.yaml')
 
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-                    # launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
-                    launch_arguments={'params_file': gazebo_params_file}.items()
-             )
+        PythonLaunchDescriptionSource([os.path.join(
+            get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+        # launch_arguments={'extra_gazebo_args': '--ros-args --params-file ' + gazebo_params_file}.items()
+        launch_arguments={
+            'params_file': gazebo_params_file}.items()
+    )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
@@ -56,19 +60,18 @@ def generate_launch_description():
                                    '-entity', 'agv'],
                         output='screen')
 
-
-    diff_drive_spawner = Node(package="controller_manager", executable="spawner.py",
+    diff_drive_spawner = Node(package="controller_manager", executable="spawner",
                               arguments=["diff_cont"],
-    )
+                              )
 
     delayed_diff_drive_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(target_action=spawn_entity, on_exit=[diff_drive_spawner],)
+        event_handler=OnProcessExit(
+            target_action=spawn_entity, on_exit=[diff_drive_spawner],)
     )
 
-    joint_broad_spawner = Node(package="controller_manager", executable="spawner.py",
+    joint_broad_spawner = Node(package="controller_manager", executable="spawner",
                                arguments=["joint_broad"],
-    )
-
+                               )
 
     # Launch them all!
     return LaunchDescription([
